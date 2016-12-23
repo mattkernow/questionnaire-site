3d8fc8defc8fc38ebb3f405a2c815fa98f3592e5
Matt <matthew.walsh@thinkwhere.com>
2016-12-23 13:17:15 +0000

    Changed questionnaire url to be root

--- questionnaire_app/urls.py
+++ questionnaire_app/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls')),
-    url(r'^home/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url('^login/$', auth_views.login, {'template_name': 'registration/login.html'}),
-    url('^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'}),
-    url('^change-password/$', auth_views.password_change, {'template_name': 'registration/password_change.html'}),
-    url('^password-change-done/&', auth_views.password_change_done, {'template_name': 'registration/password_change_done.html'})
+    url('^logout/$', auth_views.logout, {'template_name': 'registration/logged_out.html'}),
+    url('^change-password/$', auth_views.password_change, {'template_name': 'registration/password_change.html',
+                                                           'post_change_redirect': '/password-change-done'}),
+    url('^password-change-done/$', auth_views.password_change_done, {'template_name': 'registration/password_change_done.html'}),
 ]
--- questionnaire_site/urls.py
+++ questionnaire_site/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^questionnaire/', include('questionnaire_app.urls')),
+    url(r'^', include('questionnaire_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
