820726eb594bd660365cc5fc1cdef2d320f90716
Matt <matthew.walsh@thinkwhere.com>
2016-12-25 12:06:33 +0000

    Displayed queryset of questionniares

--- questionnaire_app/views.py
+++ questionnaire_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from questionnaire_app.models import Questionnaire
 
 
 def index(request):
@@ -7,4 +8,6 @@ def index(request):
     :param request: request object
     :return: rendered template
     """
-    return render(request, 'index.html')
+    filter_questionnaires = Questionnaire.objects.filter(is_open=True).order_by('end_date')
+    questionnaires = {'questionnaires': filter_questionnaires}
+    return render(request, 'index.html', context=questionnaires)
