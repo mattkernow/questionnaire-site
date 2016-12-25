ce5cd79e86104ee7b45023f56b1a5357141e9eca
Matt <matthew.walsh@thinkwhere.com>
2016-12-25 23:07:46 +0000

    Added dependancies for foundation

--- questionnaire_site/settings.py
+++ questionnaire_site/settings.py
@@ -132,4 +132,8 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, "static")
+]
