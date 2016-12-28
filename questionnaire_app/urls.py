4ff4710b61d3680b8a0e1b334f9ee7ddf206df3d
Matt <matthew.walsh@thinkwhere.com>
2016-12-28 19:33:14 +0000

    change password custom form

--- questionnaire_app/urls.py
+++ questionnaire_app/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     url('^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
     url('^logout/$', auth_views.logout, {'template_name': 'registration/logged_out.html'}),
     url('^change-password/$', auth_views.password_change, {'template_name': 'registration/password_change.html',
-                                                           'post_change_redirect': '/password-change-done'}),
+                                                           'post_change_redirect': '/password-change-done'},
+        name='change_pw'),
     url('^password-change-done/$', auth_views.password_change_done,
         {'template_name': 'registration/password_change_done.html'}),
 ]
